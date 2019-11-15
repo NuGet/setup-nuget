f1689c657e3daaa9be150e7a1e43f5c51581fe6b
Kat Marchán <kzm@zkat.tech>
2019-11-14 21:49:01 -0800

    refactor: switch to typed-rest-client and refactor installer into own module

---
+++ src/installer.ts
@@ -0,0 +1,29 @@
+import * as core from '@actions/core';
+import * as tc from '@actions/tool-cache';
+import * as fs from 'fs';
+import * as path from 'path';
+import pickVersion from './pick-version';
+
+export default async function install(spec = 'latest') {
+  const tool = await pickVersion(spec);
+  core.debug(`Found NuGet version: ${tool.version}`);
+  let cachePath = await tc.find('nuget.exe', tool.version);
+  if (!cachePath) {
+    const nugetExePath = await tc.downloadTool(tool.url);
+    cachePath = await tc.cacheFile(
+      nugetExePath,
+      'nuget.exe',
+      'nuget.exe',
+      tool.version
+    );
+  }
+  core.debug(`nuget.exe cache path: ${cachePath}.`);
+  core.exportVariable('NUGET', `${cachePath}/nuget.exe`);
+  if (process.platform !== 'win32') {
+    core.debug(`Creating dummy 'nuget' script.`);
+    const scriptPath = path.join(cachePath, 'nuget');
+    fs.writeFileSync(scriptPath, `#!/bin/sh\nmono ${cachePath}/nuget.exe $@`);
+    fs.chmodSync(scriptPath, '755');
+  }
+  core.addPath(cachePath);
+}
